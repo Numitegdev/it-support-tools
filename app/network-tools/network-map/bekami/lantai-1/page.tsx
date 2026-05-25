65ed5324d4dd4baba333679a4da9f25198e966af
Numitegdev <numiteg.itchannel@gmail.com>
2026-05-25 13:51:44 +0700

    Update page.tsx

--- app/network-tools/network-map/bekami/lantai-1/page.tsx
+++ app/network-tools/network-map/bekami/lantai-1/page.tsx
@@ -44,7 +44,7 @@ export default function Page() {
 
     <NetworkMapViewer
 
-      model="/models/Bekami_L1.glb"
+      model="/models/bekamil1.glb"
 
       onObjectClick={
         handleObjectClick
