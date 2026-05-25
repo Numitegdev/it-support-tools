96a2a14f7c3d090862043f048d0d4739bafe7579
Numitegdev <numiteg.itchannel@gmail.com>
2026-05-25 13:45:26 +0700

    Update network-map-viewer.tsx

--- components/network-map-viewer.tsx
+++ components/network-map-viewer.tsx
@@ -314,7 +314,7 @@ export default function NetworkMapViewer({
 
     onClick={() =>
       window.location.href =
-        "/network-tools/network-map/bekami/lantai-1"
+        "/network-tools/network-map"
     }
 
     className="
